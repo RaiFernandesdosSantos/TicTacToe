784253540280da793e2ac73f8d76f481d99a8784
Rai Fernandes dos Santos <raifernandes076@gmail.com>
2024-02-19 21:52:20 -0400

    Jogo "Terminado"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let jogador = 1;
+let x = [];
+let o = [];
+let somaX = 0;
+let somaO = 0;
 
-const escolha = document.querySelector(".escolha");
 const tabuleiro = document.querySelectorAll(".btnCasas");
 const reset = document.querySelector("#reset");
 const divs = document.querySelectorAll(".casa");
@@ -9,21 +12,31 @@ reset.addEventListener("click", () => {
   window.location.reload();
 });
 
-escolha.addEventListener("change", () => {
-  jogador = escolha.value;
-});
-
 tabuleiro.forEach((casas, index) => {
   casas.addEventListener("click", () => {
     casas.disabled = true;
     if (jogador === 1) {
       casas.textContent = "X";
       jogador = 2;
+      x.push(index);
+      somaX += index;
+
+      checarVencedor();
     } else if (jogador === 2) {
       casas.textContent = "O";
       jogador = 1;
+      o.push(index);
+      somaO += index;
+
+      checarVencedor();
     }
   });
 });
 
-function checarVencedor() {}
+function checarVencedor() {
+  if (x.length > 2 && somaX % 3 === 0) {
+    console.log("X venceu");
+  } else if (o.length > 2 && somaO % 3 === 0) {
+    console.log("O venceu");
+  }
+}
