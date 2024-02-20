fc6b2fe25cbec4eec040027e327975de363b679c
Rai Fernandes dos Santos <raifernandes076@gmail.com>
2024-02-19 20:41:29 -0400

    Reset do tabuleiro

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 let jogador = 1;
 
 const escolha = document.querySelector(".escolha");
+const tabuleiro = document.querySelectorAll(".btnCasas");
+const reset = document.querySelector("#reset");
+const divs = document.querySelectorAll(".casa");
+
+reset.addEventListener("click", () => {
+  window.location.reload();
+});
 
 escolha.addEventListener("change", () => {
   jogador = escolha.value;
 });
 
-const tabuleiro = document.querySelectorAll(".casas");
-
-tabuleiro.forEach((casas) => {
+tabuleiro.forEach((casas, index) => {
   casas.addEventListener("click", () => {
+    casas.disabled = true;
     if (jogador === 1) {
       casas.textContent = "X";
       jogador = 2;
@@ -19,3 +25,5 @@ tabuleiro.forEach((casas) => {
     }
   });
 });
+
+function checarVencedor() {}
